e0db66ec3429521622e9436399f39852d85759c1
magu <magumilk1436@gmail.com>
2026-01-18 21:36:33 +0900

    Refactor decryption to reuse the encryption algorithm

--- src/caesar_cipher/decryption.py
+++ src/caesar_cipher/decryption.py
@@ -1,4 +1,5 @@
 from caesar_cipher.utils import shift_char, is_lower_alpha
+from caesar_cipher.encryption import encrypt
 
 def decrypt(text: str, key: int) -> str:
     """シーザー暗号を用いて文字列を復号する.
@@ -10,10 +11,4 @@ def decrypt(text: str, key: int) -> str:
     Returns:
         str: 復号された文字列.
     """
-    plaintext = ""
-    for char in text:
-        if is_lower_alpha(char):
-            plaintext += shift_char(char, -key).upper()
-            continue
-        plaintext += char
-    return plaintext
+    return encrypt(text, -key).upper()
