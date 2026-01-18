f80a0fab3468a9bd6548066412270b9e9c5cb65f
magu <magumilk1436@gmail.com>
2026-01-18 19:33:14 +0900

    Add docstrings to classes and methods in the `shift_scale` module

--- src/components/shift_scale.py
+++ src/components/shift_scale.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 
 class ShiftScale(tk.Frame):
+    """
+    シフト数を設定するスケールウィジェット
+    """
     __LABEL_TEXT = "シフト数"
     def __init__(self, master: tk.Misc):
+        """
+        コンストラクタ
+
+        Args:
+            master (tk.Misc): 親ウィジェット
+        """
         super().__init__(master)
         self.__scale_var = tk.IntVar(value=0)
         self.__label = tk.Label(
@@ -23,4 +32,10 @@ class ShiftScale(tk.Frame):
         self.__scale.pack(side="top", fill="both", expand=True)
     
     def get_shift(self) -> int:
+        """
+        シフト数を取得する
+
+        Returns:
+            int: シフト数
+        """
         return self.__scale_var.get()
