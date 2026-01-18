d8f67dfb35f8dbfd67160c63e558789d7347f043
magu <magumilk1436@gmail.com>
2026-01-18 19:14:58 +0900

    Add doc comments to the `LabeledTextBox` class and its methods

--- src/components/labeled_textbox.py
+++ src/components/labeled_textbox.py
@@ -1,7 +1,17 @@
 import tkinter as tk
 
 class LabeledTextBox(tk.Frame):
+    """
+    ラベル付きテキストボックスウィジェット
+    """
     def __init__(self, master: tk.Misc, title: str | None = None):
+        """
+        コンストラクタ
+
+        Args:
+            master (tk.Misc): 親ウィジェット
+            title (str, optional): ラベルのテキスト. 省略時は `None`.
+        """
         super().__init__(
             master,
         )
@@ -17,4 +27,10 @@ class LabeledTextBox(tk.Frame):
         self.__textbox.pack(side="top", fill="both", expand=True)
     
     def get_text(self) -> str:
+        """
+        テキストボックスに記述された文字列を取得する
+
+        Returns:
+            str: 取得した文字列
+        """
         return self.__textbox.get("1.0", "end")
