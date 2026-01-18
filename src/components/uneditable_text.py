e5ba1ed00e2ace033694159f02a309e6c97fb902
magu <magumilk1436@gmail.com>
2026-01-18 19:34:27 +0900

    Add docstrings to classes and methods in the `uneditable_text` module

--- src/components/uneditable_text.py
+++ src/components/uneditable_text.py
@@ -1,10 +1,25 @@
 import tkinter as tk
 
 class UneditableText(tk.Text):
+    """
+    読み取り専用のテキストボックスウィジェット
+    """
     def __init__(self, master: tk.Misc):
+        """
+        コンストラクタ
+
+        Args:
+            master (tk.Misc): 親ウィジェット
+        """
         super().__init__(master, state="disabled")
     
     def set_text(self, text: str):
+        """
+        テキストをテキストボックスに設定する
+
+        Args:
+            text (str): 設定するテキスト
+        """
         self.configure(state="normal")
         self.delete("1.0", "end")
         self.insert("1.0", text)
